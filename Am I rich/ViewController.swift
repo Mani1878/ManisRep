e670d66e6be18390d32af9fde5664690fede66b0
Manuel <manuel.zarka89@gmail.com>
2019-12-24 16:29:35 +0100

    Add all files

---
+++ Am I rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Am I rich
+//
+//  Created by Manuel Zarka on 23.12.19.
+//  Copyright © 2019 Manuel Zarka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
